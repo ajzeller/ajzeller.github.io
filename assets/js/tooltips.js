29a0bfc1d6ff4763e197853a3a0bc2a520c68a9a
Andrew <andrew.j.zeller@gmail.com>
2016-06-16 22:44:20 -0700

    Initial upload

---
+++ assets/js/tooltips.js
@@ -0,0 +1,133 @@
+(function($) {
+    
+    $(document).ready(function() {
+            $('#triathlete').tooltipster({
+               animation: 'fall',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p>Universty of Alabama Club Triathlon Team</p>'),
+               interactive: true,
+                
+            });
+        });
+    
+    $(document).ready(function() {
+            $('#about2').tooltipster({
+               animation: 'fade',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p>SolidWorks &nbsp;&bull;&nbsp; Autodesk Inventor &nbsp;&bull;&nbsp; MATLAB</p>'),
+               interactive: true
+                
+            });
+        });
+    
+    $(document).ready(function() {
+            $('#about4').tooltipster({
+               animation: 'fade',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p>President of a FIRST Robotics Team &nbsp;&bull;&nbsp; Alabama Astrobotics Design Co-Lead &nbsp;&bull;&nbsp; UA ASME Secretary</p>'),
+               interactive: true
+                
+            });
+        });
+    
+    $(document).ready(function() {
+            $('#about5').tooltipster({
+               animation: 'fade',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p>Adobe After Effects &nbsp;&bull;&nbsp; Autodesk 3D Studio Max &nbsp;&bull;&nbsp; GIMP &nbsp;&bull;&nbsp; Inkscape &nbsp;&bull;&nbsp; HTML &nbsp;&bull;&nbsp; CSS</p>'),
+               interactive: true
+                
+            });
+        });
+
+    $(document).ready(function() {
+            $('#work1').tooltipster({
+               animation: 'fade',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p><a href="http://alabamaastrobotics.com" target="_blank" >NASA Robotic Mining Competition Team @ The University of Alabama</a><br>Design, produce, and program a robot capable of performing a simulated 10 minute mission <br> including navigation of an obstacle-laden environment, mining, storing, and unloading BP-1, a lunar simulant. <br>Challenges include balancing weight, cost, and functionality while navigating unpredictable terrain using autonomy and managing dust.</p>'),
+               interactive: true,
+                
+            });
+        });
+    
+    $(document).ready(function() {
+            $('#work5').tooltipster({
+               animation: 'fade',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p><a href="http://frc1716.com" target="_blank">FIRST Robotics Team 1716</a><br>Intense 6 week build season with challenges like soccer, frisbee, and basketball. <br> Worked with 40 of my high school peers to design, build, and program a fully-functioning robot.</p>'),
+               interactive: true,
+                
+            });
+        });
+    
+    $(document).ready(function() {
+            $('#work6').tooltipster({
+               animation: 'fade',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p>As a summer intern at Fetch Robotics, I designed and prototyped (from scratch!)<BR> a <a href="http://fetchrobotics.com" target="_blank">vacuum gripper</a> to be used on the end of a robotic arm for manipulation of objects. <BR> I also designed and produced several tools for production of mechanical and electrical components.</p>'),
+               interactive: true,
+                
+            });
+        });
+
+    $(document).ready(function() {
+            $('#likewhatyousee').tooltipster({
+               animation: 'grow',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p>I am interested in helping you with design consulting <i class="fa fa-smile-o"></i></p>'),
+               interactive: true
+                
+            });
+        });
+    
+    $(document).ready(function() {
+            $('#sendmessage').tooltipster({
+               animation: 'fade',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p>You will hear from me soon!</p>'),
+               interactive: true
+                
+            });
+        });
+    
+    $(document).ready(function() {
+            $('#blog-icon').tooltipster({
+               animation: 'fade',
+               delay: 300,
+               theme: 'tooltipster-default',
+               touchDevices: false,
+               trigger: 'hover',
+               content: $('<p>Blog</p>'),
+               interactive: true
+                
+            });
+        });
+    
+})(jQuery);
