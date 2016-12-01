f3f947d9342091fefac202e4c21750f37f67133f
Andrew <andrew.j.zeller@gmail.com>
2016-11-30 16:50:38 -0800

    jquery link https

--- assets/js/nav_scroll.js
+++ assets/js/nav_scroll.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
    $(document).scroll(function() {
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $("#nav").css('background-color', 'rgba(166, 171, 174, 0.7)');
+          $("#nav").css('transition', '.5s ease');
+          $("#nav").css('background', 'rgba(90, 110, 150, .5)');
        } else {
           $('#nav').css('background-color', 'transparent');
        }
