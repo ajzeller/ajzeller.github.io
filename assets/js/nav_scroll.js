a250b40f1b35f8df2a33c26225014caf3962d7f7
Andrew <andrew.j.zeller@gmail.com>
2016-12-24 23:33:33 -0600

    Added work experience dropdowns and portfolio popups
    Initial addition of work section with hover-dropdown internship
    descriptions and added a popup for each of the portfolio items with
    Jquery Popup Overlays.

--- assets/js/nav_scroll.js
+++ assets/js/nav_scroll.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
           $("#nav").css('transition', '.5s ease');
-          $("#nav").css('background', 'rgba(90, 110, 150, .5)');
+          $("#nav").css('background', 'rgba(0, 0, 0, .2)');
        } else {
           $('#nav').css('background-color', 'transparent');
        }
